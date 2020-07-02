c00b31161a1e69c9c4dd6dad59ba9f5e66bc0ba9
dewashepherd <65743039+dewashepherd@users.noreply.github.com>
2020-07-02 22:34:51 +0200

    Implemented Interfaces and bastract classes for Domain.Core

---
+++ MicroRabbitTest1/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        public Event() 
+        {
+            Timestamp = DateTime.Now;
+        
+        }
+    }
+}
